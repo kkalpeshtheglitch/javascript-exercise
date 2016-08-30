70703c212ce3a894998ba638e921747c84eea076
macbook <macbook@macbooks-MacBook-Pro.local>
2016-08-30 13:59:11 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// how to creat the alert box
+
+// function myAlert() {
+// 	alert("Hello world");
+// }
+
+// how to set conlose log
+
+//var x = 5;
+//var y = 6;
+//var z = x * y;
+
+//console.log(z);
+
+// how to set Arrey
+
+//var cars = ["Zen", "Alto", "Maruti"];
+
+//document.getElementById("car-name").innerHTML = cars;
+
+//setTimeout(cars, 2000);
+
+
+// get popup with javascript
+
+//var myPopup = document.getElementById("popup");
+
+//var triggerPopup = document.getElementById("trigger-popup");
+
+//var closeButton = document.getElementClassName("close-popup");
+
+
+// triggerPopup.onclick = function(){
+// 	myPopup.style.display = "block";
+// }
+
+// function simpleText(){
+// 	document.write("kalpesh gautam khandagale");
+// }
+
+// onload = function() {
+// 	setTimeout(simpleText, 2000);
+// }
+
+// document.getElementById("demo").innerHTML = "kalpesh khandagale" + " " + "harshala khandagale";
